9bd9d9701e03aadf27bf511a16e30591167a8030
Huy Doan <doanvanhuydhdtu@gmail.com>
2024-12-30 12:20:36 +0700

    updated

---
+++ LoggingDemo/LoggingDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace LoggingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using ILoggerFactory factory = LoggerFactory.Create(
+                builder =>
+                {
+                    builder.AddConsole();
+                    builder.SetMinimumLevel(LogLevel.None);
+                }
+            );
+            ILogger logger = factory.CreateLogger("Program1");
+
+            logger.LogTrace("LogTrace");
+            logger.LogDebug("LogDebug");
+            logger.LogInformation("LogInformation");
+            logger.LogWarning("LogWarning");
+            logger.LogError("LogError");
+            logger.LogCritical("LogCritical");
+
+            if (logger.IsEnabled(LogLevel.Debug))
+            {
+                logger.LogDebug("LogDebug:enable");
+            }
+
+
+        }
+    }
+}
